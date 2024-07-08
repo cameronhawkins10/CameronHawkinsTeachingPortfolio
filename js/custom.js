d9e82971b7dc2403513ec04fd68e1db08e6cf1bf
cameron.hawkins10 <cameron.hawkins10@gmail.com>
2024-07-07 21:52:24 -0400

    other stuff

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,30 @@
+(function ($) {
 
-  (function ($) {
-  
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
-    });
+  // PRE LOADER
+  $(window).load(function () {
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+  });
 
-    // CUSTOM LINK
-    $('.custom-link').click(function(){
+  // CUSTOM LINK
+  $('.custom-link').click(function () {
     var el = $(this).attr('href');
     var elWrapped = $(el);
     var header_height = $('.navbar').height() + 10;
 
-    scrollToDiv(elWrapped,header_height);
+    scrollToDiv(elWrapped, header_height);
     return false;
 
-    function scrollToDiv(element,navheight){
+    function scrollToDiv(element, navheight) {
       var offset = element.offset();
       var offsetTop = offset.top;
-      var totalScroll = offsetTop-navheight;
+      var totalScroll = offsetTop - navheight;
 
       $('body,html').animate({
-      scrollTop: totalScroll
+        scrollTop: totalScroll
       }, 300);
-  }
-});
-    
-  })(window.jQuery);
+    }
+  });
 
-
+})(window.jQuery);
